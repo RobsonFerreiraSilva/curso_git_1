ba7fc13486060894d8f36a1eb9c33c987e9eb40a
Robson <robsonferreira244@gmail.com>
2025-12-09 11:02:08 -0300

    adicionei mais uma linha no código

--- curso_git_github.py
+++ curso_git_github.py
@@ -3,3 +3,4 @@ nome = input("Digite seu nome: ")
 print(f"Olá {nome}, bem vindo ao curso de Git e GitHub!")
 
 print("Este é o meu primeiro repositório no GitHub.")
+print("Estou aprendendo a usar controle de versão com Git.")
